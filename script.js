90cd96b38bd801a3f1edebf83584ed13ada52e20
wallisonstorck <storck779@gmail.com>
2021-11-19 14:10:29 -0400

    finish moveToFavorites

--- script.js
+++ script.js
@@ -1,63 +1,92 @@
-let COUNTRIES_GENERAL;
-let COUNTRIES_FAVORITES;
+let countries = {
+  GENERAL: null,
+  FAVORITES: null,
+};
 
-function removeFromFavorite(id) {
-  console.log(id);
+let countriesFavorites = [];
+let countriesGeneral = [];
+
+let classNames = {
+  DIV_GENERAL: null,
+  H2_GENERAL: null,
+  SPAN_GENERAL: null,
+  UL_GENERAL: null,
+
+  DIV_FAVORITES: null,
+  H2_FAVORITES: null,
+  SPAN_FAVORITES: null,
+  UL_FAVORITES: null,
+};
+
+function removeFromFavorites(id) {
+  const i = countriesGeneral.findIndex((country) => {
+    return parseInt(country.id) == id;
+  });
+  countriesFavorites.splice(i, 1);
+}
+
+function removeFromGeneral(id) {
+  countriesGeneral.splice(id, 1);
+  showCountries(countriesGeneral);
 }
 
 function moveToFavorite(id) {
-  const divFavorites = document.querySelector(".favoriteCountries");
-  const h2Favorites = divFavorites.querySelector("h2");
-  const spanFavorites = divFavorites.querySelector("span");
-  const ulFavorites = divFavorites.querySelector(".favoriteCountry");
-
-  const i = COUNTRIES_GENERAL.findIndex((country) => {
-    return parseInt(country.id) == id;
+  const i = countriesGeneral.findIndex((country) => {
+    return parseInt(country.id) === id;
   });
 
-  ulFavorites.innerHTML += `<li>
-    <a class="btn-floating btn-small waves-effect waves-light red" onclick="removeFromFavorite(${COUNTRIES_GENERAL[i].id})"><i class="material-icons">-</i></a>
-      <img src=${COUNTRIES_GENERAL[i].flag} alt="Bandeira">
-      ${COUNTRIES_GENERAL[i].name}
-      <br>${COUNTRIES_GENERAL[i].population}
+  countriesFavorites.push(countriesGeneral[i]);
+
+  classNames.UL_FAVORITES.innerHTML = ""; //Limpa a ul
+  countriesFavorites.forEach((country) => {
+    classNames.UL_FAVORITES.innerHTML += `<li>
+    <a class="btn-floating btn-small waves-effect waves-light red" onclick="removeFromFavorites(${country.id})"><i class="material-icons">-</i></a>
+    <img src=${country.flag} alt="Bandeira">
+    ${country.name}
+    <br>${country.population}
     </li>`;
+  });
+
+  removeFromGeneral(i);
+
+  //Conta a população
+  console.log(countriesFavorites);
+  const countPopulation = countriesFavorites.reduce((accumulator, current) => {
+    return (accumulator += current.population);
+  }, 0);
+
+  classNames.H2_FAVORITES.textContent = `Países (${countriesFavorites.length})`;
+  classNames.SPAN_FAVORITES.textContent = `População total: ${countPopulation}`;
 }
 
-function showCountries(data) {
-  const divGeneral = document.querySelector(".allCountries");
-  const h2General = divGeneral.querySelector("h2");
-  const spanGeneral = divGeneral.querySelector("span");
-  const ulGeneral = divGeneral.querySelector(".generalCountry");
-  // const ulFavorite = document.querySelector(".favoriteCountry");
-
+function showCountries() {
   //Lista países
-  data.forEach((country) => {
-    ulGeneral.innerHTML += `<li>
-    <a class="btn-floating btn-small waves-effect waves-light" onclick="moveToFavorite(${country.id})"><i class="material-icons">+</i></a>
+  classNames.UL_GENERAL.innerHTML = ""; //Limpa a ul
+  countriesGeneral.forEach((country) => {
+    // console.log(`${country.name} => ${country.id}`);
+    classNames.UL_GENERAL.innerHTML += `<li>
+    <a class="btn-floating btn-small waves-effect waves-light" onclick="moveToFavorite(${parseInt(
+      country.id
+    )})"><i class="material-icons">+</i></a>
       <img src=${country.flag} alt="Bandeira">
       ${country.name}
       <br>${country.population}
     </li>`;
   });
 
-  // data.forEach((country) => {
-  //   console.log(country.id);
-  // });
-
   //Conta a população
-  const countPopulation = data.reduce((accumulator, current) => {
+  const countPopulation = countriesGeneral.reduce((accumulator, current) => {
     return (accumulator += current.population);
   }, 0);
 
-  h2General.textContent = `Países (${data.length})`;
-  spanGeneral.textContent = `População total: ${countPopulation}`;
+  classNames.H2_GENERAL.textContent = `Países (${countriesGeneral.length})`;
+  classNames.SPAN_GENERAL.textContent = `População total: ${countPopulation}`;
 }
 
 async function getDataAPI() {
-  // const data = await (await fetch("https://restcountries.com/v3.1/all")).json();
   const data = await (await fetch("https://restcountries.com/v2/all")).json();
 
-  COUNTRIES_GENERAL = data.map((country) => {
+  countriesGeneral = data.map((country) => {
     return {
       id: country.numericCode,
       name: country.name,
@@ -66,10 +95,24 @@ async function getDataAPI() {
     };
   });
 
-  showCountries(COUNTRIES_GENERAL);
+  showCountries();
+}
+
+function init() {
+  classNames.DIV_GENERAL = document.querySelector(".allCountries");
+  classNames.H2_GENERAL = document.querySelector(".cntCountriesGeneral");
+  classNames.SPAN_GENERAL = document.querySelector(".cntPopulationGeneral");
+  classNames.UL_GENERAL = document.querySelector(".generalCountry");
+
+  classNames.DIV_FAVORITES = document.querySelector(".favoriteCountries");
+  classNames.H2_FAVORITES = document.querySelector(".cntCountriesFavorites");
+  classNames.SPAN_FAVORITES = document.querySelector(".cntPopulationFavorites");
+  classNames.UL_FAVORITES = document.querySelector(".favoriteCountry");
+
+  getDataAPI();
 }
 
 window.onload = () => {
   console.log("Script is running!");
-  getDataAPI();
+  init();
 };
